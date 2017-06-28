3d82f4b16257bf78d31810e6e159a93b71efd90d
Ivan Belyavtsev <djbelyak@gmail.com>
2017-06-28 15:42:15 +0300

    Добавлена форма issue #3

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -1,11 +1,26 @@
 <template>
     <div class="contacts">
         <div class="container">
+            <form class="form-inline">
+                <label class="sr-only" for="inlineFormInput">Name</label>
+                <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Имя">
+    
+                <label class="sr-only" for="inlineFormInputGroup">Username</label>
+                <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+                    <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Email">
+                </div>
+      
+                <button type="submit" class="btn btn-primary">Добавить</button>
+            </form>
+
+
+
+
             <table class="table">
                 <thead>
                     <tr>
                         <th>Имя</th>
-                        <th>email</th>
+                        <th>Email</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -42,5 +57,8 @@ export default {
 </script>
 
 <style>
-
+.form-inline {
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
 </style>
