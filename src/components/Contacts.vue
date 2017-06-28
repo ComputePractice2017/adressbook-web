4825c7c3350a157c0722468721bf1c4948355159
Ivan Belyavtsev <djbelyak@gmail.com>
2017-06-28 15:33:10 +0300

    Список контактов issue #2

---
+++ src/components/Contacts.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="contacts">
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Имя</th>
+                        <th>email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="contact in contacts">
+                        <td>{{contact.name}}</td>
+                        <td>{{contact.email}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>  
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      contacts: [
+        {
+          'id': 1,
+          'name': 'Alice',
+          'email': 'alice@company.org'
+        },
+        {
+          'id': 2,
+          'name': 'Bob',
+          'email': 'bob@company.org'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
